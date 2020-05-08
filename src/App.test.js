e93652dda8f9ef9206146fabd84a0408ef21f814
Chance Whitaker <chancellor_whitak21@mymail.eku.edu>
2020-05-08 07:05:37 -0400

    added grey lines to odd table entries

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import Enzyme, { shallow, mount } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App, { Search, Button, Table } from './App';
 
@@ -14,18 +14,7 @@ describe('App', () => {
     ReactDOM.render(<App />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
-  it('renders the Table component', () => {
-    const wrapper = mount(<App />);
-    expect(wrapper.find(Table).length).toEqual(1);
-  });
-  it('renders the Search component', () => {
-    const wrapper = mount(<App />);
-    expect(wrapper.find(Search).length).toEqual(1);
-  });
-  it('renders four Button components', () => {
-    const wrapper = mount(<App />);
-    expect(wrapper.find(Button).length).toEqual(4);
-  });
+  
 });
 
 describe('Search', () => {
